02a89e7c4549a377c05bbb7c0c2924df368c6206
manelzada <emmanoelsveiga@gmail.com>
2022-03-02 11:32:59 -0300

    1.2.1

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const app = require("express")();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.send('servidor ligado');
-});
+  res.send("Servidor ligado")
+})
 
 io.on('connection', socket => {
-  chatID = socket.handshake.query.chatID;
-  socket.join(chatID);
-
-  socket.on('send_message', message => {
-    receiverChatId = message.receiverChatId;
-    senderChatId = message.senderChatId;
-    content = message.content
-
-    socket.in(receiverChatId).emit('receive_message', {
-      'content': content,
-      'senderChatId': senderChatId,
-      'receiverChatId': receiverChatId,
-    });
-
-  });
+  chatID = socket.handshake.query.chatID
+  socket.join(chatID)
 
   socket.on('disconnect', () => {
-    socket.leave(chatID);
-  });
+    socket.leave(chatID)
+  })
 
+  socket.on('send_message', message => {
+    receiverChatID = message.receiverChatID
+    senderChatID = message.senderChatID
+    content = message.content
+
+    socket.in(receiverChatID).emit('receive_message', {
+      'content': content,
+      'senderChatID': senderChatID,
+      'receiverChatID': receiverChatID,
+    })
+  })
 });
 
-http.listen(3000);
+http.listen(process.env.PORT)
