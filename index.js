0ef62e141080a3d11f67008433382edac5ff218e
manelzada <emmanoelsveiga@gmail.com>
2022-03-01 21:38:26 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.get('/', (req, res) => {
+  res.send('servidor ligado');
+});
+
+io.on('connection', socket => {
+  chatID = socket.handshake.query.chatID;
+  socket.join(chatID);
+
+  socket.on('disconnect', () => {
+    socket.leave(chatID);
+  });
+
+  socket.on('send_message', message => {
+    receiverChatId = message.receiverChatId;
+    senderChatId = message.senderChatId;
+    content = message.content
+
+    socket.in(receiverChatId).emit('receive_message', {
+      'content': content,
+      'senderChatId': senderChatId,
+      'receiverChatId': receiverChatId,
+    });
+
+  });
+
+});
+
+http.listen(3000);
