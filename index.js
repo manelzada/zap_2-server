6013a4742a2e4fd71eabf7f672b81b1bebb99977
manelzada <emmanoelsveiga@gmail.com>
2022-03-05 00:38:03 -0300

    Histórico de Mensagems

--- index.js
+++ index.js
@@ -4,20 +4,27 @@ const socketIO = require('socket.io')(server)
 socketIO.on('connection', function (client) {
   console.log('Conectado', client.id);
 
+  let messages = [];
+
+  client.on('messageHistory', function name(data) {
+    console.log(messages);
+    //socketIO.emit('messageHistory', messages);
+  });
+
   client.on('message', function name(data) {
-    console.log(data);
+    messages.push(data);
     socketIO.emit('message', data);
-  })
+  });
 
   client.on('disconnect', function () {
     console.log('Desconectado', client.id);
-  })
+  });
 
   client.on('error', function (err) {
     console.log('Error detected', client.id);
     console.log(err);
-  })
-})
+  });
+});
 
 var port = process.env.PORT || 3000;
 
